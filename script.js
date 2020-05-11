129eda05bafdb340ef07a405769dde07fa6c9ab5
sigge94 <sigge.94@live.se>
2020-05-11 20:43:11 +0200

    slider

--- script.js
+++ script.js
@@ -6,3 +6,28 @@ function scrollToAboutMe(){
     element.scrollIntoView({block: "end"});
     element.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
 }
+
+function sliderHome(){
+    document.getElementById('homecontainer').style.width = "100%";
+    document.getElementById('maincontainer').style.left = "100%";
+    document.getElementById('portfoliocontainer').style.left = "100%";
+}
+
+function sliderAbout(){
+    document.getElementById('homecontainer').style.width = "0";
+
+    document.getElementById('maincontainer').style.width = "100%";
+    document.getElementById('maincontainer').style.left = "0";
+
+    document.getElementById('portfoliocontainer').style.left = "100%";
+    
+}
+
+function sliderPortfolio(){
+    document.getElementById('homecontainer').style.width = "0";
+
+    document.getElementById('maincontainer').style.width = "0";
+    document.getElementById('maincontainer').style.left = "0";
+
+    document.getElementById('portfoliocontainer').style.left = "0";
+}
